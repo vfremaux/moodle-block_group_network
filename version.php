0c9c1ef1a22c108cb7fbc311c69646c971d2a0a2
valery <valery.fremaux@gmail.com>
2018-03-23 10:57:02 +0100

    Fix travais

--- version.php
+++ version.php
@@ -27,11 +27,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2013041600;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2016120500;        // Requires this Moodle version.
+$plugin->requires  = 2017050500;        // Requires this Moodle version.
 $plugin->component = 'block_group_network'; // Full name of the plugin (used for diagnostics).
-$plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '3.2.0 (build 2013041600)';
+$plugin->maturity = MATURITY_RC;
+$plugin->release = '3.3.0 (build 2013041600)';
 $plugin->dependencies = array('block_user_mnet_hosts' => '*');
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.2.0000';
+$plugin->codeincrement = '3.3.0000';
