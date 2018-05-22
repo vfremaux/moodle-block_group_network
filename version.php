e531f3149056cbe44e87b707440d669b5809b4f5
valery <valery.fremaux@gmail.com>
2018-05-22 13:49:44 +0200

    Pushing new M35 versions

--- version.php
+++ version.php
@@ -27,11 +27,11 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version   = 2013041600;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2017050500;        // Requires this Moodle version.
+$plugin->requires  = 2018042700;        // Requires this Moodle version.
 $plugin->component = 'block_group_network'; // Full name of the plugin (used for diagnostics).
 $plugin->maturity = MATURITY_RC;
-$plugin->release = '3.3.0 (build 2013041600)';
+$plugin->release = '3.5.0 (build 2013041600)';
 $plugin->dependencies = array('block_user_mnet_hosts' => '*');
 
 // Non moodle attributes.
-$plugin->codeincrement = '3.3.0000';
+$plugin->codeincrement = '3.5.0000';
